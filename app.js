14a7070a0feb4b951756315efa46e1c240355462
panascharoenruk <63070107@kmitl.ac.th>
2024-07-09 15:46:52 +0700

    change forms array to object

--- app.js
+++ app.js
@@ -23,21 +23,21 @@ app.get("/getQuestion", (req, res) => {
 });
 //get question by id
 app.get("/getQuestionById/:id", (req, res) => {
-  console.log(forms[0].Form.question[(parseInt(req.params.id)-1)]);
-  res.json(forms[0].Form.question[(parseInt(req.params.id)-1)]);
+  console.log(forms[0].Form.question[(parseInt(req.params.id) - 1)]);
+  res.json(forms[0].Form.question[(parseInt(req.params.id) - 1)]);
 });
 
 //create
 app.post("/createForms", (req, res) => {
   //loop question
-  for(let i = 0; i < req.body.Form.question.length; i++){
+  for (let i = 0; i < req.body.Form.question.length; i++) {
     let checkChoice = [];
     //loop choice
-    for(let j = 0; j < req.body.Form.question[i].choice.length; j++){
+    for (let j = 0; j < req.body.Form.question[i].choice.length; j++) {
       checkChoice.push(req.body.Form.question[i].choice[j].correct);
     }
     //if choice have more than 1 correct answer
-    if(checkChoice.filter(Boolean).length > 1){
+    if (checkChoice.filter(Boolean).length > 1) {
       //return error
       res.send({
         message: "There shouldn't have more than one correct answer!"
@@ -54,12 +54,12 @@ app.post("/createForms", (req, res) => {
 
 //Update Form Data
 //what if pressed save and update whole form
-app.put("/forms", (req, res) => {
+app.put("/updateForms", (req, res) => {
   //clear old form
   forms = [];
-  
+
   forms.push(req.body);
-  
+
   res.send(forms);
 });
 
