357f07832e02ed290a86c2bafb1fab6516407e41
panascharoenruk <63070107@kmitl.ac.th>
2024-07-09 17:43:21 +0700

    fixed getQuestion

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const app = express()
 const port = 3000
 
 var forms = {};
-
+let questions = [];
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.json(forms);
 })
 //get all
 app.get("/getForms", (req, res) => {
@@ -17,12 +17,29 @@ app.get("/getForms", (req, res) => {
 });
 //get all question
 app.get("/getQuestion", (req, res) => {
-  res.json(forms.Form.question);
+  try{
+    questions = forms.Form.question;
+    res.json(questions);
+  } catch(err) {
+    res.send(forms);
+  }
+
 });
 //get question by id
 app.get("/getQuestionById/:id", (req, res) => {
-  console.log(forms.Form.question[(parseInt(req.params.id) - 1)]);
-  res.json(forms.Form.question[(parseInt(req.params.id) - 1)]);
+  
+  try{
+    if(forms.Form.question.length >= parseInt(req.params.id)){
+      console.log(forms.Form.question[(parseInt(req.params.id) - 1)]);
+      res.json(forms.Form.question[(parseInt(req.params.id) - 1)]);
+    } else {
+      res.send({
+        message: "Question didn't exist"
+      });
+    }
+  } catch(err){
+    res.send(err);
+  }
 });
 
 //create
@@ -35,26 +52,22 @@ app.post("/createForms", (req, res) => {
     //loop choice
     for (let j = 0; j < req.body.Form.question[i].choice.length; j++) {
       checkChoice.push(req.body.Form.question[i].choice[j].correct);
-    }
-    //if choice have more than 1 correct answer
-    if (checkChoice.filter(Boolean).length > 1) {
-      //response error message
-      res.send({
-        message: "There shouldn't have more than one correct answer!"
-      });
-      return null;
+      //if choice have more than 1 correct answer
+      if (checkChoice.filter(Boolean).length > 1) {
+        //response error message
+        res.send({
+          message: "There shouldn't have more than one correct answer!"
+        });
+        return null;
+      }
     }
     checkChoice = [];
   }
-
   forms = req.body;
   res.send(forms);
-  
-  
 });
 
 /* //Update Form Data
-//what if pressed save and update whole form
 app.put("/updateForms", (req, res) => {
   //clear old form
   forms = {};
